2c9d4fe8c4faec32221b8f2bbfb4c3475ccbb5d0
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-10-07 16:05:00 -0300

    new actualization

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -1,13 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
-
-// Carga las variables de entorno
-dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'leonardo1',
+  api_key: '341779722638824',
+  api_secret: 'sBq4S6HumeqUrgHY_hg-J2N8Yjc',
 });
 
 export default cloudinary;
--- backend/src/config/hola.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'leonardo1',
-  api_key: '341779722638824',
-  api_secret: 'sBq4S6HumeqUrgHY_hg-J2N8Yjc',
-});
-
-export default cloudinary;
